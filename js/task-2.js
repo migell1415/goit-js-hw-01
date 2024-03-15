75a41ab7ff8144b22ac4a9391fb197a7863ae466
migell1415 <154928249+migell1415@users.noreply.github.com>
2024-03-15 20:36:22 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
-    return `"Shipping to ${country} will cost ${totalPrice} credits"`;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
 getShippingMessage("Australia", 120, 50);
